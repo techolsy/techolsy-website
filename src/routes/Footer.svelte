48ecff04b680e6107d03cdf63dd9bb297eee8fcb
techolsy <alexander@techolsy.eu>
2025-02-16 21:48:14 +0100

    Footer.svelte: added aria-labelledby

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -7,10 +7,10 @@
     <p>&copy Techolsy 2025</p>
   </div>
   <div class="align-right">
-    <a href="https://github.com/techolsy" target="_blank">
+    <a href="https://github.com/techolsy" target="_blank" aria-labelledby="github">
       <i class="fa-brands fa-square-github fa-xl icon"></i>
     </a>
-    <a href="https://linkedin.com/in/alexander-olsson-111b92177" target="_blank">
+    <a href="https://linkedin.com/in/alexander-olsson-111b92177" target="_blank" aria-labelledby="linkedin">
       <i class="fa-brands fa-linkedin fa-xl icon"></i>
     </a>
   </div>
