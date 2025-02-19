9598ff08288cfa673173bb2b60cbfb8297a8a3f0
techolsy <alexander@techolsy.eu>
2025-02-19 18:55:10 +0100

    Moved icons from footer to header

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,27 +1,3 @@
-<script>
-  import '@fortawesome/fontawesome-free/css/all.min.css'
-</script>
-
-<div class=grid>
-  <div>
-    <p>&copy Techolsy 2025</p>
-  </div>
-  <div class="align-right">
-    <a href="https://github.com/techolsy" target="_blank" aria-labelledby="github">
-      <i class="fa-brands fa-square-github fa-xl icon"></i>
-    </a>
-    <a href="https://linkedin.com/in/alexander-olsson-111b92177" target="_blank" aria-labelledby="linkedin">
-      <i class="fa-brands fa-linkedin fa-xl icon"></i>
-    </a>
-  </div>
+<div>
+  <p>&copy Techolsy 2025</p>
 </div>
-
-<style>
-  .icon {
-    color: #77767b;
-  }
-
-  .align-right {
-    margin-left: auto; 
-  }
-</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,12 +1,24 @@
 <script>
+  import '@fortawesome/fontawesome-free/css/all.min.css'
+
   const site_pages = ['Home', 'About', 'Homelab'];
 </script>
 
 <nav>
   <ul>
     <li><strong>Techolsy</strong></li>
+    <li>
+      <a href="https://github.com/techolsy" target="_blank" aria-labelledby="github">
+        <i class="fa-brands fa-square-github fa-xl icon"></i>
+      </a>
+    </li>
+    <li>
+      <a href="https://linkedin.com/in/alexander-olsson-111b92177" target="_blank" aria-labelledby="linkedin">
+        <i class="fa-brands fa-linkedin fa-xl icon"></i>
+      </a>
+    </li>
   </ul>
-  <ul>
+    <ul>
     {#each site_pages as site_page}
       <li>
         {#if site_page === 'Home'}
@@ -18,3 +30,9 @@
     {/each}
   </ul>
 </nav>
+
+<style>
+  .icon {
+    color: #77767b;
+  }
+</style>
