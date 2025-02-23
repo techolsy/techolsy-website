ad6f8fd335feffaf471f532b31fe20ece12c51a5
techolsy <alexander@techolsy.eu>
2025-02-23 23:49:56 +0100

    Networking.svelte: styled with tailwind

--- src/routes/homelab/Networking.svelte
+++ src/routes/homelab/Networking.svelte
@@ -2,43 +2,41 @@
   import { writable } from 'svelte/store';
   import { SvelteFlow, Background, Controls, Handle } from '@xyflow/svelte';
   import '@xyflow/svelte/dist/style.css';
-  import CustomNode from './CustomNode.svelte';
+
+  import CustomNode from '$lib/components/CustomNode.svelte';
+  import Accordion from '$lib/components/Accordion.svelte';
+
+  const pTags = "text-xl py-1";
 
   const nodes = writable([
     {
       id: '1', 
-      position: { x: 320, y: 50 },
-      type: 'customNode',
+      position: { x: 350, y: 50 },
       data: { label: 'netgate SG-1100' },
     },
     {
       id: '2',
       position: { x: 350, y: 150},
-      type: 'customNode',
       data: { label: 'MokerLink' },
     },
     {
       id: '3',
       position: { x: 650, y: 300},
-      type: 'customNode',
       data: { label: 'UniFi AC Lite'}
     },
     {
       id: '4',
       position: { x: 450, y: 300},
-      type: 'customNode',
       data: { label: 'PVE01'}
     },
     {
       id: '5',
       position: { x: 250, y: 300},
-      type: 'customNode',
       data: { label: 'PVE02'}
     },
     {
       id: '6',
       position: { x: 50, y: 300},
-      type: 'customNode',
       data: { label: 'PBS'}
     },
   ]);
@@ -55,8 +53,8 @@
       target: '3',
     },
     { 
-      id: '2-4', 
-      source: '2', 
+      id: '2-4',
+      source: '2',
       target: '4',
     },
     { 
@@ -76,31 +74,25 @@
   };
 </script>
 
-<h3>Networking</h3>
-<p>
+<h3 class="text-3xl">Networking</h3>
+
+<p class="{ pTags }">
   I have a very simple networking setup, it consists of one gateway, one switch and one access point.
   as for the gateway is a <i>netgate SG 1100</i> running pfSense, the switch is a <i>MokerLink 2G080110GSM</i> 
   and the access point is a <i>UniFi AP Lite</i>.
 </p>
-<p>
+
+<p class="{ pTags }">
   The gateway is connected to my ISP and I have created an extra vlan that I am using for IOT devices. 
   The switch is not doing much extra more than tagging along the extra vlan to my AP and having my wired pc's connected.
   All my wireless devices are connected to my AP, and it serves both my networks I have created. 
   Below is a simple diagram showing the topology of how everything is connected.
 </p>
 
-<details>
-  <summary>Network Diagram</summary>
-  <div class="diagram">
-    <SvelteFlow {nodes} {edges} {nodeTypes} fitView>
-      <Background bgColor="rgba(32,38,50,1)" patternColor="rgba(1,170,255,0.5)" />
-      <Controls />
-    </SvelteFlow>
-  </div>
-</details>
-
-<style>
-  .diagram {
-    height: 50vh;
-  }
-</style>
+<div class="py-3">
+  <Accordion title="Network Diagram">
+    <div class="h-[400px]">
+      <SvelteFlow {nodes} {edges} fitView />
+    </div>
+  </Accordion>
+</div>
