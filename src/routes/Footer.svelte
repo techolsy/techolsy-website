4047ad9b1eb8643f1f6dd14c7df620c9e7b71cbd
techolsy <alexander@techolsy.eu>
2025-02-23 23:46:15 +0100

    Footer.svelte: styled with tailwind and added social icons

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,3 +1,19 @@
-<div>
-  <p>&copy Techolsy 2025</p>
+<script>
+  import SocialIcons from '@rodneylab/svelte-social-icons';
+</script>
+
+<div class="flex w-full bg-gray-600">
+  <div class="flex justify-between container mx-auto py-2">
+    <div class="flex items-center justify-center">
+      <p class="text-lg">&copy Techolsy 2025</p>
+    </div>
+    <div class="flex">
+      <a href="https://linkedin.com/in/alexander-olsson-111b92177" target="_blank">
+        <SocialIcons alt="" network="linkedin" width="40" height="40" />
+      </a>
+      <a href="https://github.com/techolsy" target="_blank" class="ml-3">
+        <SocialIcons alt="" network="github" width="40" height="40" bgColor="#111111" />
+      </a>
+    </div>
+  </div>
 </div>
