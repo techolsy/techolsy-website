ae4b36b625641115ed0248b578540238714ba94e
techolsy <alexander@techolsy.eu>
2025-02-23 23:47:11 +0100

    about page: styled with tailwind

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,22 +1,29 @@
+<script>
+  const pTags = "text-xl py-1";
+</script>
+
 <svelte:head>
   <title>About</title>
 </svelte:head>
 
-<h1>About Me</h1>
+<div class="container mx-auto py-5">
+  <h1 class="text-4xl">About Me</h1>
+  <br>
 
-<p>
-  <i>Hej!</i> I’m a Swedish systems engineer with a passion for problem solving, efficiency, 
-  and all things tech. By day, I design and optimize systems that keep businesses running smoothly. 
-  Whether it's software architecture, automation, or troubleshooting complex issues, 
-  I thrive on finding innovative solutions to technical challenges.
-</p>
-<p>
-  But when the workday ends, you'll often find me immersed in music. 
-  Whether I'm playing the guitar, experimenting with new melodies, 
-  or simply unwinding with my favorite tracks, music is my creative escape. 
-  It balances the structure of engineering with the freedom of expression, giving me the best of both worlds.
-</p>
-<p>
-  Outside of tech and music, I enjoy exploring new ideas, collaborating with like-minded people, 
-  and continuously learning because there’s always something new to discover.
-</p>
+  <p class="{ pTags }">
+    <i>Hej!</i> I’m a Swedish systems engineer with a passion for problem solving, efficiency, 
+    and all things tech. By day, I design and optimize systems that keep businesses running smoothly. 
+    Whether it's software architecture, automation, or troubleshooting complex issues, 
+    I thrive on finding innovative solutions to technical challenges.
+  </p>
+  <p class="{ pTags }">
+    But when the workday ends, you'll often find me immersed in music. 
+    Whether I'm playing the guitar, experimenting with new melodies, 
+    or simply unwinding with my favorite tracks, music is my creative escape. 
+    It balances the structure of engineering with the freedom of expression, giving me the best of both worlds.
+  </p>
+  <p class="{ pTags }">
+    Outside of tech and music, I enjoy exploring new ideas, collaborating with like-minded people, 
+    and continuously learning because there’s always something new to discover.
+  </p>
+</div>
