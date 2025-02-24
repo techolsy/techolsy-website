20eadb3d4cb38a64b8eff4ee5a169432fef1f0a2
techolsy <alexander@techolsy.eu>
2025-02-24 18:44:40 +0100

    Footer.svelte: made the icons smaller

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -3,16 +3,16 @@
 </script>
 
 <div class="flex w-full bg-gray-600">
-  <div class="flex justify-between container mx-auto py-2">
+  <div class="flex justify-between container mx-auto py-4">
     <div class="flex items-center justify-center">
       <p class="text-lg">&copy Techolsy 2025</p>
     </div>
     <div class="flex">
       <a href="https://linkedin.com/in/alexander-olsson-111b92177" target="_blank">
-        <SocialIcons alt="" network="linkedin" width="40" height="40" />
+        <SocialIcons alt="" network="linkedin" width="30" height="30" />
       </a>
       <a href="https://github.com/techolsy" target="_blank" class="ml-3">
-        <SocialIcons alt="" network="github" width="40" height="40" bgColor="#111111" />
+        <SocialIcons alt="" network="github" width="30" height="30" bgColor="#111111" />
       </a>
     </div>
   </div>
