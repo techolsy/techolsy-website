79a295aeecd6a11a7a6884dd1e471801ce122934
techolsy <alexander@techolsy.eu>
2025-02-25 22:02:07 +0100

    Header.svelte: changed colors

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,13 +3,13 @@
 
   const sitePages = ['Home', 'About', 'Projects','Homelab'];
   const menuClass = "text-lg no-underline hover:text-blue-500 ml-3";
-  const menuActive = "text-lg no-underline text-blue-400 hover:text-blue-500 ml-3";
+  const menuActive = "text-lg no-underline text-blue-600 hover:text-blue-500 ml-3";
 </script>
-<div class="w-full bg-gray-700">
+<div class="w-full bg-gray-950">
   <div class="container mx-auto">
     <nav class="flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 sm:items-baseline w-full">
       <div class="mb-2 sm:mb-0">
-        <a href="/" class="text-2xl no-underline text-gray-200 hover:text-blue-300">Techolsy</a>
+        <a href="/" class="text-2xl no-underline text-gray-200 hover:text-blue-600">Techolsy</a>
       </div>
       <div>
         {#each sitePages as sitePage}
