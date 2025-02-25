9d8b5d541800d82e1a30caceaf1e2317d2da2f14
techolsy <alexander@techolsy.eu>
2025-02-25 22:01:50 +0100

    Footer.svelte: changed colors

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,17 +2,17 @@
   import SocialIcons from '@rodneylab/svelte-social-icons';
 </script>
 
-<div class="flex w-full bg-gray-600">
+<div class="flex w-full bg-gray-950">
   <div class="flex justify-between container mx-auto py-4">
     <div class="flex items-center justify-center">
-      <p class="text-lg">&copy Techolsy 2025</p>
+      <p class="text-lg text-gray-400">&copy Techolsy 2025</p>
     </div>
     <div class="flex">
       <a href="https://linkedin.com/in/alexander-olsson-111b92177" target="_blank">
         <SocialIcons alt="" network="linkedin" width="30" height="30" />
       </a>
       <a href="https://github.com/techolsy" target="_blank" class="ml-3">
-        <SocialIcons alt="" network="github" width="30" height="30" bgColor="#111111" />
+        <SocialIcons alt="" network="github" width="30" height="30" bgColor="#99a1af" />
       </a>
     </div>
   </div>
