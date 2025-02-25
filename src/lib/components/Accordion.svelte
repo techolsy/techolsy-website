3f1318982d9ce7962ee4258faf4fc98cb496227a
techolsy <alexander@techolsy.eu>
2025-02-25 22:01:12 +0100

    Accordion.svelte: changed colors

--- src/lib/components/Accordion.svelte
+++ src/lib/components/Accordion.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="border border-gray-400 rounded-lg overflow-hidden">
-  <button class="w-full flex justify-between items-center bg-gray-700 p-4 text-left cursor-pointer" on:click={() => isOpen = !isOpen}>
+  <button class="w-full flex justify-between items-center bg-gray-800 p-4 text-left cursor-pointer" on:click={() => isOpen = !isOpen}>
     <span class="font-semibold">{ title }</span>
     <span class="transition-transform transform {isOpen ? 'rotate-180' : 'rotate-0'}">▼</span>
   </button>
