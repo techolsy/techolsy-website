2e9b493b865701c8f0cdf9b4386c2dec428c747c
techolsy <alexander@techolsy.eu>
2025-02-26 19:34:13 +0100

    CustomNode.svelte: fixed styling with tailwind

--- src/lib/components/CustomNode.svelte
+++ src/lib/components/CustomNode.svelte
@@ -3,30 +3,8 @@
   export let data;
 </script>
 
-<div class="custom-node">
+<div class="flex bg-gray-900 shadow-lg rounded-lg border border-gray-400 w-38 h-9">
   <Handle type="target" position="top" />
-  <div>{data.label}</div>
+  <div class="flex flex-grow text-gray-200 items-center justify-center">{data.label}</div>
   <Handle type="source" position="bottom" />
 </div>
-
-<style>
-  .custom-node {
-    padding: 10px;
-    background: white;
-    border-radius: 5px;
-    text-align: center;
-    border: 1px solid #ccc;
-  }
-
-  :global(.svelte-flow__handle) {
-    width: 6px !important;
-    height: 6px !important;
-    background-color: #007bff !important;
-    border-radius: 50% !important;
-    border: none !important;
-    padding: 0 !important;
-    margin: 0 !important;
-    box-shadow: none !important;
-    display: block !important;
-  }
-</style>
