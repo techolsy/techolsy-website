5838ab13c7a5590acf8be000a1b73a4f85685fd0
techolsy <alexander@techolsy.eu>
2025-02-27 17:48:18 +0100

    Button.svelte: added

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let name = "Button";
+  export let color = "primary";
+  export let url = "#"
+
+  const buttonColors = {
+    primary: "blue",
+    secondary: "red",
+    success: "green"
+  };
+</script>
+
+<div class={`flex py-2 px-2 items-center justify-center border rounded-xl cursor-pointer bg-${buttonColors[color]}-500 hover:bg-${buttonColors[color]}-400 text-white border-gray-200`}>
+  <a href="{url}" class="font-semibold">
+    {name}
+  </a>
+</div>
