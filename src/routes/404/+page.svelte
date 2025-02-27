e7fc5a7f389cb6f2331b72338c3d94662fa683ee
techolsy <alexander@techolsy.eu>
2025-02-27 17:48:29 +0100

    404 page: added

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Button from "$lib/components/Button.svelte";
+</script>
+
+<svelte:head>
+  <title>404 - Page Not Found</title>
+  <meta name="404 - Page Not Found" content="404 - Page Not Found"> 
+</svelte:head>
+
+<div class="flex flex-col flex-grow items-center justify-center">
+  <h1 class="text-4xl text-red-500">404 - Page Not Found</h1>
+  <p class="text-lg text-gray-400">The page you are looking for does not exist</p>
+  <Button 
+    name="Go back Home"
+    url="/"
+    color="primary"/>
+</div>
